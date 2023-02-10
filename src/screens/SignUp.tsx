3e6d75e9597936d4d08065c39f3958e3f420a9d1
George W F Santos <gweverton21@hotmail.com>
2023-02-10 11:13:00 -0300

    improve button spacing in the Signup form

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -124,7 +124,7 @@ export function SignUp() {
         <Button
           title="Voltar para o login"
           variant="outline"
-          mt={24}
+          mt={12}
           onPress={goBack}
         />
       </VStack>
